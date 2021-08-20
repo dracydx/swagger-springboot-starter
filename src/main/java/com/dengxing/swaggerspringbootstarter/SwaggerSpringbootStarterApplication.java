602c0b848861e78afbaf80882a5801bf4f2d4629
dengxing <374294495@qq.com>
2021-08-20 15:17:05 +0800

    init

---
+++ src/main/java/com/dengxing/swaggerspringbootstarter/SwaggerSpringbootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.dengxing.swaggerspringbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerSpringbootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerSpringbootStarterApplication.class, args);
+    }
+
+}
